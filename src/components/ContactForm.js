98149d8f11077d93a68169ea5fce7059a87fe8e5
kirkkwang <k3wang@gmail.com>
2022-02-06 07:21:47 -0800

    contact form works

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,50 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import "./ContactForm.css";
 
-export default class ContactForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      message: "",
-    };
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mgedarol");
+  if (state.succeeded) {
+    return <p>Thanks for the message!</p>;
   }
+  return (
+    <form className="ContactForm" onSubmit={handleSubmit}>
+      <div className="ContactForm__Input__FirstName">
+        <label className="ContactForm__Label__FirstName">first name</label>
+        <input id="first-name" type="first-name" name="first-name" />
+      </div>
+      <div className="ContactForm__Input__LastName">
+        <label className="ContactForm__Label__LastName">last name</label>
+        <input id="last-name" type="last-name" name="last-name" />
+      </div>
 
-  handleChange = (input) => (e) => {
-    this.setState({
-      [input]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    console.log(this.state);
-    e.preventDefault();
-  };
-
-  render() {
-    const { handleChange, handleSubmit } = this;
-    return (
-      <form className="ContactForm" onSubmit={handleSubmit}>
-        <div className="ContactForm__Input__FirstName">
-          <label className="ContactForm__Label__FirstName">first name</label>
-          <input type="text" onChange={handleChange("firstName")} />
-        </div>
-        <div className="ContactForm__Input__LastName">
-          <label className="ContactForm__Label__LastName">last name</label>
-          <input type="text" onChange={handleChange("lastName")} />
-        </div>
-        <div className="ContactForm__Input__Email">
-          <label className="ContactForm__Label__Email">email</label>
-          <input type="text" onChange={handleChange("email")} />
-        </div>
-        <div className="ContactForm__TextArea__Message">
-          <label className="ContactForm__Label__Message">message</label>
-          <textarea onChange={handleChange("message")}></textarea>
-        </div>
-        <button>submit</button>
-      </form>
-    );
-  }
+      <div className="ContactForm__Input__Email">
+        <label className="ContactForm__Label__Email" htmlFor="email">
+          email
+        </label>
+        <input id="email" type="email" name="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div className="ContactForm__TextArea__Message">
+        <label className="ContactForm__Label__Message" htmlFor="message">
+          message
+        </label>
+        <textarea id="message" name="message" />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <button
+        className="ContactForm__Submit"
+        type="submit"
+        disabled={state.submitting}
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
+function App() {
+  return <ContactForm />;
+}
+export default App;
